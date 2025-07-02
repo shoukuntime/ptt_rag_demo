d89e3ab7d2b544b2f286326d46a768402736dc06
jenjiunchen <jenjiunchen@eland.com.tw>
2025-07-02 09:06:04 +0800

    Day13

---
+++ env_settings.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent
+
+class EnvSettings(BaseSettings):
+    GOOGLE_API_KEY : str = None
+    PINECONE_API_KEY : str = None
+    PINECONE_INDEX_NAME : str = None
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / '.env',
+        env_file_encoding='utf-8',
+        extra='ignore'
+    )
+
+
